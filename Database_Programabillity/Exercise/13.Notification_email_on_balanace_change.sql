e7b577a368fc5242ece1465539ebaff7585c99a9
svetl <svetlin.sii@gmail.com>
2025-02-06 21:06:04 +0200

    updated solution

--- Database_Programabillity/Exercise/13.Notification_email_on_balanace_change.sql
+++ Database_Programabillity/Exercise/13.Notification_email_on_balanace_change.sql
@@ -1,11 +1,3 @@
-CREATE TABLE notification_emails(
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
-    recipient_id int,
-    subject varchar(50),
-    body text
-);
-
-
 CREATE OR REPLACE FUNCTION trigger_fn_send_email_on_balance_change()
 RETURNS TRIGGER AS
 
@@ -15,11 +7,15 @@ $$
 
     BEGIN
 
-        subject := 'Balance change for account: ' || NEW.account_id;
-        body := 'On ' || '2023-08-19' || ' ' || 'your balance was changed from ' || new.old_sum|| ' to ' || new.new_sum;
+--         subject := 'Balance change for account: ' || NEW.account_id;
+--         body := 'On ' || '2023-08-19' || ' ' || 'your balance was changed from ' || new.old_sum|| ' to ' || new.new_sum;
 
         INSERT INTO notification_emails (recipient_id, subject, body)
-    VALUES (new.account_id ,subject ,body);
+        VALUES (
+                   OLD.id,
+                   format('Balance change for account: %s', NEW.id),
+                   format('On %s your balance was changed from %s to %s', to_char(now(), 'YYYY-MM-DD'), OLD.old_sum, NEW.new_sum)
+               );
 
     RETURN NEW ;
 
